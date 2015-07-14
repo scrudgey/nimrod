c9ac04b7084ffb9043e4f77393a7755ba4adad73
RSHAW <scrudgey@gmail.com>
2015-07-14 11:19:12 -0700

    added default interpretation capability to grammar class.

--- nimrod/nimrod.py
+++ nimrod/nimrod.py
@@ -1,41 +1,49 @@
 # -*- coding: utf-8 -*-
 """Main routines for parsing specially-formatted context-free grammars and databases for the purpose
 of generating general, randomized text.
-# """
-# import os
+"""
 import re
 import random
 
-#1. interpret and get a string back
-#2. check the string for any further parsing. if not, done
-#3. if further parsing, increase depth counter and go to 1.
-
 class grammar(object):
-
+  """Base class contains the definitions for a series of grammatical symbols.
+  It can interpret a symbol, which is a random process, or parse a string,
+  which replaces each symbol with an interpretation.
+  """
   symbol_hook = re.compile(r'(\{(.+?)\})')
 
   def __init__(self):
     self.symbols = {}
 
-  def interpret(self, symbol):
+  def interpret(self, symbol, raw=False):
     """Return a random element from the dictionary for a given symbol.
     """
     if symbol in self.symbols:
-      return random.choice(self.symbols[symbol])
+      if not raw:
+        return self.parse(random.choice(self.symbols[symbol]))
+      else:
+        return random.choice(self.symbols[symbol])
     else:
       return None
 
   def parse(self, string, depth=0):
     """This is the main routine where the magic happens.
+    Replace every instance of {symbol} in the string with a randomized entry
+    from that symbol's definition.
     """
     initial_string = string
     for match in self.symbol_hook.finditer(string):
-      # print "%s: %s" % (match.start(), match.group(2))
       string = string.replace(match.group(1), self.interpret(match.group(2)), 1)
     if initial_string != string and depth < 10:
       return self.parse(string, depth=depth + 1)
     else:
       return string
+
+  def run(self):
+    """Interpret the default string, which is defined in a dictionary
+    as #default.
+    """
+    print self.interpret('default')
 
 def load(path):
   """Load the grammar definition at path into a new dictionary.
@@ -54,8 +62,5 @@ def load(path):
         items = gram.symbols.get(current_key, [])
         items.append(line)
         gram.symbols[current_key] = items
-
-        # gram.symbols[current_key] = gram.symbols.get(current_key, []).append(line)
-  print gram.parse('{noun} is {noun}')
   return gram
 
