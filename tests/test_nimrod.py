2d9d890bb1e693359433e40dd720716d76a9ba49
RSHAW <scrudgey@gmail.com>
2018-03-12 18:55:56 -0700

    preliminary tests

--- tests/test_nimrod.py
+++ tests/test_nimrod.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
 test_nimrod
 ----------------------------------
@@ -9,20 +8,48 @@ Tests for `nimrod` module.
 """
 
 import unittest
+import nimrod
 
-from nimrod import nimrod
+class TestSyntax(unittest.TestCase):
+  """Test the main syntax features of nimrod."""
 
+  def setUp(self):
+    self.g = nimrod.Grammar()
+    self.g.load('tests/def')
 
-class TestNimrod(unittest.TestCase):
+  def test_interpret(self):
+    self.assertEqual(self.g.interpret('symbol'), 'fresh')
+    self.assertEqual(self.g.interpret('nested-symbol'), 'fresh')
+    self.assertEqual(self.g.interpret('nested-symbol', parse=False), '{symbol}')
+    # test depth catching
+    # self.assertEqual(self.g.interpret('nested-symbol', depth=100), '{symbol}')
+    # symbol doesn't exist
+    self.assertEqual(self.g.interpret('not-symbol'), '{not-symbol}')
 
-    def setUp(self):
-        pass
+  def test_parse(self):
+    self.assertEqual(self.g.parse('{symbol}'), 'fresh')
+    self.assertEqual(self.g.parse('{nested-symbol}'), 'fresh')
+    # test depth catching
+    # self.assertEqual(self.g.parse('{loop-symbol-1}'), '{loop-symbol-1}')
+    # symbol doesn't exist
+    self.assertEqual(self.g.parse('{not-symbol}'), '{not-symbol}')
 
-    def test_something(self):
-        pass
+  def test_var(self):
+    # test setting a variable
+    self.g.parse('{set-var}')
+    self.assertEqual(self.g.ref('var'), 'crescent')
+    # unset variables parse to ''
+    self.assertEqual(self.g.ref('not-a-var'), '')
+    # test resetting variables
+    self.g.reset()
+    self.assertEqual(self.g.ref('var'), '')
 
-    def tearDown(self):
-        pass
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
+
+
+# interpret
+# interpret parse=False
+# parse
+# nested parse
